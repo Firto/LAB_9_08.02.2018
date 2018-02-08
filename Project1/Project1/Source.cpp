8b40da427a485325ede4a9ca4e1250b8a893f1f6
prochliste <prochliste@gmail.com>
2018-02-08 16:26:06 +0200

    VPR_5_Написати рекурсивну функцію, яка отримує масив і знаходить кількість парних елементів

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -3,12 +3,22 @@
 #include <ctime>
 #include <string>
 using namespace std;
-// Написати рекурсивну функцію, яка отримує ціле число і переводить його у 16 ву систему числення.
-void Shistnad(long long n) {
-	if (n > 0) { Shistnad(n / 16); cout << n % 16; }
+// Написати рекурсивну функцію, яка отримує масив і знаходить кількість парних елементів
+long long sum_elsm(int mass[], int n) {
+	if (mass[n-1] % 2 == 0 && n > 0 && mass[n - 1] != 0) return (1 + sum_elsm(mass, n - 1));
+	else if(mass[n - 1] % 2 > 0 && n > 0) return (0 + sum_elsm(mass, n - 1));
+	else return 0;
 }
 void main() {
-	Shistnad(125);
+	int mass[10];
+	srand(time(0));
+	for (int i = 0; i < 10; i++)
+	{
+		mass[i] = rand() % 10;
+		cout << " " << mass[i];
+	}
+	cout << endl;
+	cout << sum_elsm(mass, 10);
 	cout << endl;
 	system("pause");
 }
