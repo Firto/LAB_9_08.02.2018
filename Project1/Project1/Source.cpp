6aaca6068b04fdb958cef1daa59c4648012d1b8b
prochliste <prochliste@gmail.com>
2018-02-08 16:16:14 +0200

    Написати рекурсивну функцію, яка отримує ціле число і переводить його у 16 ву систему числення.

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -3,13 +3,12 @@
 #include <ctime>
 #include <string>
 using namespace std;
-// Написати рекурсивну функцію яка обчислює суму массива
-long long sum(int n) {
-	if (n > 0)
-		return (n + sum(n - 1));
-	else return 0;
+// Написати рекурсивну функцію, яка отримує ціле число і переводить його у 16 ву систему числення.
+void Shistnad(long long n) {
+	if (n > 0) { Shistnad(n / 16); cout << n % 16; }
 }
 void main() {
-	cout << sum(5) << endl;
+	Shistnad(125);
+	cout << endl;
 	system("pause");
 }
