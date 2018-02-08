d218917087c452b17b78d8d8b71eaad8ecb644f7
prochliste <prochliste@gmail.com>
2018-02-08 16:46:58 +0200

    VPR_1.1_Написати рекурсивну функцію для обчислення , де – дробове число, – ціле число

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -3,28 +3,13 @@
 #include <ctime>
 #include <string>
 using namespace std;
-// Написати рекурсивну функцію для перетворення масиву: поміняти місцями перший елемент з другим, третій з четвертим, і т.д.
-void swep(int mass[], int n) {
-	n--;
-	if (n > 0){
-		swep(mass, n - 1);
-		swap(mass[n], mass[n - 1]);
-    }
+// Написати рекурсивну функцію для обчислення , де – дробове число, – ціле число
+double mst(double num, int n) {
+	if (n > 1) return mst(num, n - 1)*num;
+	else return num;
 }
 void main() {
-	int mass[10];
-	srand(time(0));
-	for (int i = 0; i < 10; i++)
-	{
-		mass[i] = rand() % 10;
-		cout << " " << mass[i];
-	}
-	cout << endl;
-	swep(mass, 10);
-	for (int i = 0; i < 10; i++)
-	{
-		cout << " " << mass[i];
-	}
+	cout << mst(5.4, 3);
 	cout << endl;
 	system("pause");
 }
